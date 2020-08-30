294798f2734042c1cf9b309919f51e3ae59ac87c
khairul <khairultaher@gmail.com>
2020-08-30 20:04:06 +0600

    cache remove

---
+++ WebApplication1/Controllers/District.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public static class District
+    {
+
+       public static List<string> Districts { get; set; }
+    }
+}
